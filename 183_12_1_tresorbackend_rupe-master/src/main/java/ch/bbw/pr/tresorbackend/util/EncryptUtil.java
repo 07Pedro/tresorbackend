92dcd2a84cc7e08b9d8839d939c7c4d302c28a7a
Petr Cerny <petr.cerny@e3ag.ch>
2025-03-25 08:54:49 +0100

    added encryption/decryption

--- 183_12_1_tresorbackend_rupe-master/src/main/java/ch/bbw/pr/tresorbackend/util/EncryptUtil.java
+++ 183_12_1_tresorbackend_rupe-master/src/main/java/ch/bbw/pr/tresorbackend/util/EncryptUtil.java
@@ -2,27 +2,48 @@ package ch.bbw.pr.tresorbackend.util;
 
 import org.jasypt.util.text.AES256TextEncryptor;
 
+import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
+import java.util.*;
+
 /**
  * EncryptUtil
  * Used to encrypt content.
  * Not implemented yet.
- * @author Peter Rutschmann
+ * @author Petr Cerny
  */
 public class EncryptUtil {
 
-   //todo ergänzen!
+   SecureRandom random = new SecureRandom();
+   AES256TextEncryptor encryptor = new AES256TextEncryptor();
+   byte[] salt = new byte[32];
+   String secretKey;
 
    public EncryptUtil(String secretKey) {
-      //todo ergänzen!
+      this.secretKey = secretKey;
    }
 
    public String encrypt(String data) {
-      //todo anpassen!
+      // random wert wird generiert und zum secret hinzugefügt
+      random.nextBytes(salt);
+      String decodedSalt = Base64.getEncoder().encodeToString(salt);
+      secretKey = secretKey + decodedSalt;
+      encryptor.setPassword(secretKey);
+
+      data = encryptor.encrypt(data);
+
       return data;
    }
 
    public String decrypt(String data) {
-      //todo anpassen!
+      List<String> encryption = Arrays.asList(data.split(":"));
+      String secret = encryption.get(0);
+      String salt = encryption.get(1);
+
+      byte[] decodedSalt = Base64.getDecoder().decode(salt);
+      encryptor.setPassword(secret + decodedSalt);
+
+      data = encryptor.decrypt(secret);
       return data;
    }
 }
